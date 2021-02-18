d19dcdaf6be4255eec20f5b6f6d42f04eb8d15ae
SimonFSmith <simfsmith@icloud.com>
2021-02-18 15:20:04 -0500

    Added Firebase to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from 'firebase/app';
+
+// Add the Firebase services that you want to use
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
 import './App.global.css';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCuFSYdHSVVgM7Ai2bgwf2bLTnS6y1_KZM',
+  authDomain: 'application-notes-6dde3.firebaseapp.com',
+  projectId: 'application-notes-6dde3',
+  storageBucket: 'application-notes-6dde3.appspot.com',
+  messagingSenderId: '1057276670354',
+  appId: '1:1057276670354:web:d0e7b2ccee347d27a7514b',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const Hello = () => {
   return (
