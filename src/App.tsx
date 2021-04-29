7d6995c73f29ffbcf5105f1541d69aad1c3bacdb
unknown <mick_2002@outlook.fr>
2021-04-29 16:52:09 -0400

    firebase-connexion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable prettier/prettier */
@@ -13,10 +15,11 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
-import Register from './Register';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.global.css';
+
+import Register from "./Register";
+import notePad from './notePad';
 
 
 // Your web app's Firebase configuration
@@ -29,52 +32,23 @@ const firebaseConfig = {
   appId: '1:1057276670354:web:d0e7b2ccee347d27a7514b',
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if(firebase.apps.length===0){
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
-const Hello = () => {
-  return (
-    <body>
-      <div id="login">
-        <h3 className="text-center text-white pt-5"></h3>
-        <div className="container">
-           <div id="login-row" className="row justify-content-center align-items-center" >
-             <div id="login-coid=lumn" className="col-md-6">
-                <div id="login-box" className="col-md-12">
-                  <form id="login-fom" className="form" action="" method="post">
-                    <h3 className="text-center text-info">Bienvenue</h3>
-                    <div className="form-goup">
-                        <label htmlFor="email" className="text-info">Adresse Courielle: </label><br/>
-                        <input type="email" name="email" id="email" className="form-control"></input>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="Mot de passe" className="text-info">Mot de passe:</label>
-                      <input type="password" name="password" id="password" className="form-control" ></input>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="remember-me" className="text-info" ><span>Rester connecté</span><span>.<input id="remember-me" name="remember-me" type="checkbox"></input></span></label><br/>
-                      <input type="submit" name="submit" className="btn btn-info btn-md" value="Connecter" ></input>
-                    </div>
-                    <div id="register-link" className="text-right">
-                      <a href="#" className="text-info">Créer un compte</a>
 
-                    </div>
-                  </form>
-                </div>
-             </div>
-           </div>
-        </div>
-      </div>
-    </body>
-  );
-};
-
-export default function App() {
+ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Register} />
         <Route path= "/Register" component={Register}/>
+        ­<Route path="/notePad" component={notePad}/>
       </Switch>
     </Router>
   );
 }
+
+export default App;
--- src/Register.tsx
+++ src/Register.tsx
@@ -6,47 +6,143 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
+import { Link, Redirect } from 'react-router-dom';
+import noteApp from './notePad';
+import firebase from 'firebase';
+
+
+export interface RegisterProps {
 
 
 
-const Register = () => {
-  return (
-    <body>
+}
+
+export interface RegisterState {
+  adresse: string,
+  password: string,
+  stayConnected: boolean
+}
+
+class Register extends React.Component<RegisterProps, RegisterState> {
+  constructor(props: RegisterProps) {
+    super(props);
+    this.state = {
+      adresse: "a",
+      password: "b",
+      stayConnected: true
+
+      };
+      this.connectionClick = this.connectionClick.bind(this);
+
+      this.emailRef = React.createRef();
+      this.passwordRef = React.createRef();
+      this.stayConnectedRef = React.createRef();
+
+
+
+  }
+
+  connectionClick(){
+
+    firebase.auth().signInWithEmailAndPassword(this.state.adresse,this.state.password)
+    .then((userCredential) => {
+
+      //CONNECTION REUSSI FAIT DE QUOI POUR PASSER A LA PROCHAINE PAGE
+      //Utiliser les routes pour passer à la page de l'app.
+      //Envoyer le ID de l'utilisateur connecté a l'app.
+    })
+    .catch((error) =>{
+      //CONNECTION PAS REUSSIE, CHANGE DE QUOI DANS LA PAGE POUR LE DIRE A LUTILISATEUR
+      //Message d'erreur pour afficher que l'utilisateur a pas reussi
+    })
+
+
+    console.log(this.state.adresse, this.state.password,
+      this.state.stayConnected);
+    console.log(noteApp)
+
+
+  }
+
+  onFieldChanged(){
+
+
+  }
+
+  render() {
+    return (
+      <body>
       <div id="login">
         <h3 className="text-center text-white pt-5"></h3>
         <div className="container">
            <div id="login-row" className="row justify-content-center align-items-center" >
              <div id="login-coid=lumn" className="col-md-6">
                 <div id="login-box" className="col-md-12">
-                  <form id="login-fom" className="form" action="" method="post">
-                    <h3 className="text-center text-info">Création de compte</h3>
+                  <div id="login-fom" className="form" >
+                    <h3 className="text-center text-info">Bienvenue</h3>
+
                     <div className="form-goup">
+
                         <label htmlFor="email" className="text-info">Adresse Courielle: </label><br/>
-                        <input type="text" name="email" id="email" className="form-control"></input>
+
+                        <input ref={this.emailRef}
+                               type="email"
+                               name="email"
+                               id="email"
+                               className="form-control"
+                               onChange={() => this.setState({adresse: this.emailRef.current.value})}
+                               />
                     </div>
                     <div className="form-group">
                       <label htmlFor="Mot de passe" className="text-info">Mot de passe:</label>
-                      <input type="text" name="password" id="password" className="form-control" ></input>
+                      <input ref={this.passwordRef}
+                             type="password"
+                             name="password"
+                             id="password"
+                             className="form-control"
+                             onChange= {() => this.setState({password: this.passwordRef.current.value})}/>
                     </div>
+
                     <div className="form-group">
-                      <label htmlFor="remember-me" className="text-info" ><span>Rester connecté</span><span><input id="remember-me" name="remember-me" type="checkbox"></input></span></label><br/>
-                      <input type="submit" name="submit" className="btn btn-info btn-md" value="submit" ></input>
+                      <label htmlFor="remember-me" className="text-info" ><span>Rester connecté</span><span>.
+                        <input id="remember-me"
+                               name="remember-me"
+                               ref={this.stayConnectedRef}
+                               type="checkbox"
+                               onChange={() =>  this.setState({stayConnected: this.stayConnectedRef.current.checked})}></input></span></label><br/>
+                      <div>
+                      <Link to="/notePad">
+                      <input
+                             type="submit"
+                             name="submit"
+                             className="btn btn-info "
+                             value="Conncter"
+                             onClick={this.connectionClick}
+                             />
+                      </Link>
+                      </div>
+
+
+
                     </div>
                     <div id="register-link" className="text-right">
-                      <a href="#" className="text-info">Revemir en arrièr</a>
+                      <a href="#" className="text-info">Créer un compte</a>
                     </div>
-                  </form>
+                  </div>
                 </div>
              </div>
            </div>
         </div>
       </div>
     </body>
-  );
-};
+    );
+  }
+}
+
 
 export default Register;
+
+
+
+
